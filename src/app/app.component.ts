4dd09d6ea6a03268a1b6f957fdd139f56d07f587
Anthony Ortega <aortega1285@mail.fresnostate.edu>
2019-05-15 15:20:47 -0700

    intro to toast alerts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastService } from './toast/toast.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toast-app';
+  randomNumber: number;
+  randomToastType: string;
+
+  constructor(private toastService: ToastService) { }
+
+  showToast() {
+    this.randomNumber = Math.floor(Math.random() * 4) + 1;
+    switch (this.randomNumber) {
+      case 1: {
+        this.randomToastType = 'info';
+        break;
+      }
+      case 2: {
+        this.randomToastType = 'success';
+        break;
+      }
+      case 3: {
+        this.randomToastType = 'warning';
+        break;
+      }
+      case 4: {
+        this.randomToastType = 'danger';
+        break;
+      }
+    }
+
+    const toastType = this.randomToastType;
+    const toastMessage = 'The random number generated is:' + ' ' + this.randomNumber;
+    const timeout = 4000;
+    this.toastService.showToast(toastType, toastMessage, timeout);
+  }
 }
